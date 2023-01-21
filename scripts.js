d8c7b8da13cba4deade082787cb2f1d84658dabc
user.name <raquellbezerraa20@gmail.com>
2023-01-21 12:48:28 -0400

    adicionei a function para manter a conexão

--- scripts.js
+++ scripts.js
@@ -6,6 +6,8 @@ function perguntaNome(){
         nomeUsuario = prompt('Qual é o seu nome?');
     }
     acessarBatePapo();
+    matemConexao();
+    buscarMensagens();
 }
 
 //função para ter acesso ao bate papo;
@@ -13,5 +15,22 @@ function acessarBatePapo(){
     const nomeDoUsuario = {name: nomeUsuario};
     const usuario = axios.post( "https://mock-api.driven.com.br/api/v6/uol/participants", nomeDoUsuario);
 }
+
+
+//função que mantém a conexão do usuário;
+function matemConexao(){
+    const conexaoDoUsuario = {name: nomeUsuario};
+    const conectado = axios.post ("https://mock-api.driven.com.br/api/v6/uol/status",conexaoDoUsuario);
+
+    conectado.then(resultado);
+    console.log(matemConexao);
+    
+}
+
+//só pra conferir se deu certo;
+function resultado(){}
+
+
+
 
 perguntaNome();
