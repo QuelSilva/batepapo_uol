51697f64706f9bf50a4a2cb2b2c5f40dec20c83b
user.name <raquellbezerraa20@gmail.com>
2023-01-21 12:50:52 -0400

    adicionei a function buscar mensagens e renderizar

--- scripts.js
+++ scripts.js
@@ -30,6 +30,30 @@ function matemConexao(){
 //só pra conferir se deu certo;
 function resultado(){}
 
+//busca pela mensagem do usuário;
+function buscarMensagens() {
+    const buscaDeMensagem = axios.get(
+      "https://mock-api.driven.com.br/api/v6/uol/messages"
+    );
+    buscaDeMensagem.then(renderizarMensagens);
+  }
+
+//função que vai fazer tudo funcionar;
+function renderizarMensagens(mensagens) {
+    const mensagem = mensagens.data;
+    let buscar = document.querySelector(".chat");
+    for (let i = 0; i < mensagem.length; i++) {
+      if (mensagem[i].type == "message") {
+        buscar.innerHTML += `<div class="mensagemParaTodos caixaMensagem"> <time>(${mensagem[i].time})</time> ${mensagem[i].from} para <strong>Todos</strong>${mensagem[i].to} ${mensagem[i].text}</div>`;
+      } else if (mensagem[i].type == "status") {
+        buscar.innerHTML += `<div class="entradaEsaida caixaMensagem"><time>(${mensagem[i].time})</time> ${mensagem[i].from} ${mensagem[i].text}</div>`;
+      } else if (mensagem[i].type == "private_message") {
+        buscar.innerHTML += `<div class="mensagemReservada caixaMensagem"><time>(${mensagem[i].time})</time> ${mensagem[i].from}Reservado para ${mensagem[i].to} ${mensagem[i].text} </div>`;
+      }
+    }
+    setInterval(buscarMensagens, 3000);
+    buscar.lastElementChild.scrollIntoView();
+  }
 
 
 
